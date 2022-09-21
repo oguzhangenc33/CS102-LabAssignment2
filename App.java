41a39a7d3b93a2f350c3d170e164c029dd22b085
kutluhann <kutluhan22@gmail.com>
2022-09-21 15:21:42 +0300

    Add diffFromAverage method

--- App.java
+++ App.java
@@ -1,7 +1,23 @@
 public class App {
+    public static int[] diffFromAverage(int[] arr) {
+        int total = 0;
+        int average;
+        int[] newArr = new int[arr.length];
+
+        for (int n : arr) {
+            total += n;
+        }
+        average = total / arr.length;
+
+        for (int i = 0; i < newArr.length; i++) {
+            newArr[i] = arr[i] - average;
+        }
+
+        return newArr;
+    }
 
     public static void main( String[] args ) {
-        
+    
     }
 
 }
