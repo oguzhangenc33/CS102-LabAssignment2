51c46185474241aedd5c994cd44bee1874e1cf04
Furkan Emre Kolukırık <furkanemre_2003@hotmail.com>
2022-09-21 15:07:42 +0300

    add min and max functions

--- App.java
+++ App.java
@@ -1,5 +1,34 @@
 public class App {
 
+    public static int getMinimum(int[] arr){
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] < min ){
+                min = arr[i];
+            }
+        }
+
+
+
+        return min;
+    }
+
+
+    public static int getMaximum(int[] arr){
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] > max ){
+                max = arr[i];
+            }
+        }
+
+
+
+        return max;
+    }
+
     public static void main( String[] args ) {
         
     }
