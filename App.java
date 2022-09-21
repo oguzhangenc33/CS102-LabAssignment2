f7d2e7991ec44d0edc344901e16684672c273cf2
Kaanayden <kaanayden15@gmail.com>
2022-09-21 15:41:35 +0300

    Combine all code pieces

--- App.java
+++ App.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class App {
     
@@ -38,6 +39,52 @@ public class App {
             }
         }
         return sum;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean exit = false;
+        int[] randArray;
+
+        do {
+            System.out.printf("""
+            Array: %s
+            1 - Set random array
+            2 - Get minimum value of array    
+            3 - Get maxiumum value of array  
+            4 - Differences from average
+            5 - Calculate sum of even indexed values
+            6 - Calculate sum of odd indexed values
+            9 - Exit
+            """, randArray == null ? Arrays.toString(randArray) : "Not generated yet.");
+            int selection = input.nextInt();
+            if( selection == 1 ) {
+                System.out.println("Enter size of the array which will be created");
+                int size = input.nextInt();
+                randArray = setArray( size );
+                System.out.println("Your random generated array: " + Arrays.toString( randArray ) );
+            } else if( selection == 2 ) {
+                int minValue = getMinimum( randArray );
+                System.out.println("Minimum number of the array: " + minValue);
+            } else if( selection == 3 ) {
+                int maxValue = getMaximum( randArray );
+                System.out.println("Maximum number of the array: " + maxValue);
+            } else if( selection == 4 ) {
+                int[] diffArray = diffFromAverage( randArray );
+            } else if( selection == 5 ) {
+                int sum = calculateSpecialSum( randArray, false );
+                System.out.println("Sum of even indexed values: " + sum);
+            }  else if( selection == 6 ) {
+                int sum = calculateSpecialSum( randArray, true );
+                System.out.println("Sum of odd indexed values: " + sum);
+            } else if( selection == 9 ) {
+                exit = true;
+                System.out.println("Exiting...");
+            } else {
+                System.out.println("This is not a valid selection.");
+            }
+
+        } while (exit == false);
+
     }
     public static int getMinimum(int[] arr){
         int min = Integer.MAX_VALUE;
